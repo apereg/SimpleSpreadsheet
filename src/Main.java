d9ca1de7034ec25dbe4a418fdaa2e2aca55f8bee
AdrianPerezGarcia <apereg24@estudiantes.unileon.es>
2019-11-09 19:29:18 +0100

    Finished refurbished Main and structured Spreadsheet

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,31 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String entry;
-		int numSheets = readNumSheets(sc);
-		int sheetJ = readNumSheets(sc);
-		int sheetI = readNumSheets(sc);
-		for (int k = 0; k < numSheets; k++) {
-			SpreadSheet spreadSheet = new SpreadSheet(sheetI, sheetJ);
-			for (int i = 0; i < sheetI; i++) {
-				entry = sc.nextLine();
-				spreadSheet.fillLine(entry.split(" "));
-			}
-			spreadSheet.resolve();
-			spreadSheet.toString();
-		}
-		
+		int numSheets = readAnInteger(sc);
+		int sheetJ = readAnInteger(sc);
+		int sheetI = readAnInteger(sc);
+		resolveSpreadsheet(sc, numSheets, sheetI, sheetJ);
+		sc.close();
 	}
 
-	private static int readNumSheets(Scanner scanner) {
+	private static int readAnInteger(Scanner scanner) {
 		int tam = scanner.nextInt();
 		scanner.nextLine();
 		return tam;
+	}
+
+	private static String[] getALine(Scanner scanner) {
+		return scanner.nextLine().split("");
+	}
+
+	private static void resolveSpreadsheet(Scanner sc, int numSheets, int sheetI, int sheetJ) {
+		for (int k = 0; k < numSheets; k++) {
+			SpreadSheet spreadSheet = new SpreadSheet(sheetI, sheetJ);
+			for (int i = 0; i < sheetI; i++) {
+				spreadSheet.fillLine(getALine(sc), i);
+			}
+			spreadSheet.resolve();
+			System.out.println(spreadSheet.toString());
+		}
 	}
 }
--- src/SpreadSheet.java
+++ src/SpreadSheet.java
@@ -1,49 +1,78 @@
 
 public class SpreadSheet {
 	
+	private String[][] entry;
+
+	private int[][] solution;
+
 	private int numRows;
 	
 	private int numCols;
-	
-	private int actualRow;
-	
-	private String[][] entry;
-	
-	private int[][] solution;
 	
 	public SpreadSheet(int i, int j) {
 		this.numRows = i;
 		this.numCols = j;
 		this.entry = new String[numRows][numCols];
 		this.solution = new int[numRows][numCols];
-		this.actualRow = 0;
-	}
-	
-	public void fillLine(String[] entry) {
-		for (int i = 0; i < entry.length; i++) {
-			try {
-				solution[this.actualRow][i] = Integer.parseInt(entry[i]);
-				this.entry[this.actualRow][i] = null;
-			}catch(NumberFormatException e) {
-				this.entry[this.actualRow][i] = entry[i];
+		for (i = 0; i < this.numRows; i++) {
+			for (j = 0; j < this.numCols; j++) {
+				entry[i][j] = "";
 			}
 		}
-		this.actualRow++;
+	}
+	
+	private boolean isANum(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		}catch(NumberFormatException e) {
+			return false;
+		}	
+	}
+
+	public void fillLine(String[] input, int row) {
+		for (int j = 0; j < this.numCols; j++) {
+			if (isANum(input[j]))
+				this.solution[row][j] = Integer.parseInt(input[j]);
+			else
+				this.entry[row][j] = input[j];
+
+		}
 	}
 
 	public void resolve() {
-		System.out.println("jaja");
-		
+		for (int i = 0; i < this.numRows; i++) {
+			for (int j = 0; j < this.numCols; j++) {
+				if(this.entry[i][j] != "") {
+					resolveFormula(this.entry[i][j]);
+					this.entry[i][j] = "Solved";
+				}
+			}
+		}
 	}
 	
-	private int getCell(String position) {
-		return 0;
+	private int resolveCell(int row, int col) {
+		if (this.entry[row][col] == "") {
+			return resolveFormula(this.entry[row][col]);
+		}
+		return this.solution[row][col];
 	}
 	
-	private void resolveCell(int i, int j) {
-		
-	}
 	
+	private int resolveFormula(String formula) {
+		formula = formula.substring(1, (formula.length()-1));
+		String[] formulaSplitted = formula.split("+");
+		int result = 0;
+		for (int i = 0; i < formulaSplitted.length; i++) {
+			for (int k = 0; k < formulaSplitted[i].length()-1; k++) {
+				if(isANum(formulaSplitted[i].substring(k, k))) {
+					
+				}
+			}
+		}
+		return result;
+	}
+
 	@Override
 	public String toString() {
 		StringBuffer out = new StringBuffer();
